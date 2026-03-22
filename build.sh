43540889f24e40606b2d6d2b79be80c44c3aa71a
k.nagateja <tejakavitapu@gmail.com>
2026-03-22 22:19:25 +0530

    Fix dlib build with cmake policies

--- build.sh
+++ build.sh
@@ -5,14 +5,17 @@ set -o errexit
 # Upgrade pip
 python -m pip install --upgrade pip
 
-# Install cmake and numpy first as they are build dependencies
-python -m pip install cmake numpy
+# Pin cmake to a known stable version for dlib
+python -m pip install "cmake<3.28" numpy
+
+# Fix for "Compatibility with CMake < 3.5 has been removed from CMake"
+# And we also add -DDUSE_AVX_INSTRUCTIONS=OFF to save memory/compatibility
+export CMAKE_ARGS="-DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DUSE_AVX_INSTRUCTIONS=OFF"
 
 # Install dlib separately with no-cache to save memory
-# dlib compilation is very memory-intensive
 python -m pip install dlib==19.24.2 --no-cache-dir
 
-# Install the rest of the requirements
+# Install the rest
 python -m pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
