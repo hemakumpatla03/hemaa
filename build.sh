b4c6f5797700265fc193ef1983acd50f61d7768e
k.nagateja <tejakavitapu@gmail.com>
2026-03-22 22:13:00 +0530

    Optimize build for dlib on Render

--- build.sh
+++ build.sh
@@ -2,7 +2,18 @@
 # exit on error
 set -o errexit
 
-pip install -r requirements.txt
+# Upgrade pip
+python -m pip install --upgrade pip
+
+# Install cmake and numpy first as they are build dependencies
+python -m pip install cmake numpy
+
+# Install dlib separately with no-cache to save memory
+# dlib compilation is very memory-intensive
+python -m pip install dlib==19.24.2 --no-cache-dir
+
+# Install the rest of the requirements
+python -m pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
